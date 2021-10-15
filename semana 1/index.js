e42d65b1cc0e66d98857a75787b2a188eda717bd
LMoralesSanchez <lmoralessnchez@gmail.com>
2021-10-15 13:49:49 -0500

    commit gitignore

---
+++ semana 1/index.js
@@ -0,0 +1,9 @@
+let numero = 10;
+
+// console.log(numero);
+// if (numero > 10 || numero === 10) {
+//   console.log(numero);
+// }
+for (let i = 0; i < numero; i++) {
+  console.log(`El valor de I es: ${i}`);
+}
