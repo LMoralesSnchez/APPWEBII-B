69f29d00d9642e58e8a90bc4306128530d167c04
LMoralesSanchez <lmoralessnchez@gmail.com>
2021-10-29 12:17:11 -0500

    commit viernes npm dotenv

--- semana 4/jueves/ejemplo.js
+++ semana 4/jueves/ejemplo.js
@@ -3,24 +3,26 @@ const mongoose = require("mongoose");
 const conecion =
   "mongodb+srv://Expo_MongoDB:expo_mongoose@cluster0.svi74.mongodb.net/Producto?retryWrites=true&w=majority";
 
-mongoose
-  .connect(conecion)
-  .then((respuesta) => {
-    console.log(respuesta);
-  })
-  .catch((error) => {
-    console.log(error);
+// .then((respuesta) => {
+//   console.log(respuesta);
+// })
+// .catch((error) => {
+//   console.log(error);
+// });
+(async () => {
+  const estado = await mongoose.connect(conecion);
+  const Usuario = mongoose.model("usuario", {
+    nombre: String,
   });
-const Usuario = mongoose.model("usuario", {
-  nombre: String,
-});
+  const Usuario1 = new Usuario({
+    nombre: "Majo",
+  });
 
-const Usuario1 = new Usuario({
-  nombre: "Leixer",
-});
+  const guardoUsuario = await Usuario1.save();
+  const resultado = await Usuario.find();
+  console.log(resultado);
+})();
 
-// Usuario1.save();
-
-Usuario.find().then((respuesta) => {
-  console.log(respuesta);
-});
+// .then((respuesta) => {
+//   console.log(respuesta);
+// });
