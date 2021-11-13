3be9a8e9ea50c92219cb19420a7b29a16f355706
LMoralesSanchez <lmoralessnchez@gmail.com>
2021-11-12 23:48:24 -0500

    jueves express about

--- semana 6/express/index.js
+++ semana 6/express/index.js
@@ -1,13 +1,43 @@
 const fs = require("fs");
 const http = require("http");
+const path = require("path");
 
 const express = require("express");
+const cors = require("cors");
 
 const PUERTO = 3000;
 
 const indice = fs.readFileSync("./index.html");
 const acercade = fs.readFileSync("./About.html");
 const error = fs.readFileSync("./error.html");
+const urlError = path.join(__dirname, "error.html");
+
+const server = express();
+
+server.use(cors()).use(express.json());
+
+server.get("/", funcionx);
+
+server.get("/About", (req, res) => {
+  // res.write(acercade);
+  // res.end();
+  res.status(200).send({
+    respesta: "Hola",
+  });
+});
+
+server.use((req, res, next) => {
+  res.status(400).sendFile(urlError);
+});
+
+function funcionx(req, res) {
+  res.write(indice);
+  res.end();
+}
+
+server.listen(PUERTO, () => {
+  console.log(`Servidor corriendo http://localhost:${PUERTO}`);
+});
 
 // http
 //   .createServer((req, res) => {
